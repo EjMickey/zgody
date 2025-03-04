f1d6b91ab5f81f9b585cda609311883d6abfd002
EjMickey <salekmikolaj2002@gmail.com>
2025-03-04 14:57:08 +0100

    new version

---
+++ imageList.js
@@ -0,0 +1,6 @@
+const imageFiles = [
+    "https://twojanazwa.github.io/zgody/images/ptok1.jpg",
+    "https://twojanazwa.github.io/zgody/images/ptok2.jpg",
+    "https://twojanazwa.github.io/zgody/images/ptok3.jpg",
+    "https://twojanazwa.github.io/zgody/images/ptok4.jpg"
+];
