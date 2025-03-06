35d8bd949ec6a603b77262ea71e9a729663e99a3
EjMickey <131490559+EjMickey@users.noreply.github.com>
2025-03-06 22:34:18 +0100

    Delete imageList.js

--- imageList.js
+++
@@ -1,6 +0,0 @@
-const imageFiles = [
-    "https://ejmickey.github.io/zgody/ptoszki/ptok1.jpg",
-    "https://ejmickey.github.io/zgody/ptoszki/ptok2.jpg",
-    "https://ejmickey.github.io/zgody/ptoszki/ptok3.jpg",
-    "https://ejmickey.github.io/zgody/ptoszki/ptok4.jpg"
-];
